18e21676b427aeca8fe2c983e275e93a9a484ee4
mateolarach <125067597+mateolarach@users.noreply.github.com>
2023-03-06 09:41:19 -0400

    solución de ejercicio 1

---
+++ ejercicio1_solucion (1).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double n1,n2;
+    int suma;
+cout << "Escriba 2 numeros: ";
+    cin >> n1; 
+    cin >> n2 ;
+suma = (n1 + n2);
+ cout << suma << endl;
+    return 0;
+}
